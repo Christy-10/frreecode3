64ef62037f0b2f94ec3c244a3eb6e8568e8e0c71
Chriee <christeena.sebastian@cloudium.io>
2021-09-17 15:24:39 +0530

    update

--- myApp.js
+++ myApp.js
@@ -74,25 +74,29 @@ const findEditThenSave = (personId, done) => {
 
   //done(null /*, data*/);
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name:personName},{age:ageToSet},{new:true},(err,updatedDoc)=>{
+    if(err) console.log(err);
+    done(null,updatedDoc);
+  })
+  //done(null /*, data*/);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const removeById = (personId, done) => {
   done(null /*, data*/);
