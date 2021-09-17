e0de58481fd4d9ae1ee6dbf1808d1b81220c77b4
Chriee <christeena.sebastian@cloudium.io>
2021-09-17 15:16:41 +0530

    d

--- myApp.js
+++ myApp.js
@@ -37,18 +37,22 @@ var createManyPeople = (arrayOfPeople, done) => {
   });
   //done(null /*, data*/);
 };
-
-
-
-
-
-
-
-
-
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+  });
 };
+
+
+
+
+
+
+
+
+
+
 
 const findOneByFood = (food, done) => {
   done(null /*, data*/);
